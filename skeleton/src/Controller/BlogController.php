73cf15f9f7c92bb3c0e7fae340e928028ba7bd10
Federico Vecco <federico.vecco@gmail.com>
2018-12-31 12:26:08 +0000

    API Platform - Introduction

--- skeleton/src/Controller/BlogController.php
+++ skeleton/src/Controller/BlogController.php
@@ -7,9 +7,13 @@
 namespace App\Controller;
 
 use App\Entity\BlogPost;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,74 +22,79 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class BlogController extends AbstractController
 {
-    private const POSTS = [
-        [
-            'id' => 1,
-            'slug' => 'hello-world',
-            'title' => 'Hello World!'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'another-post',
-            'title' => 'This is another post!'
-        ],
-        [
-            'id' => 3,
-            'slug' => 'last-example',
-            'title' => 'This is the last example'
-        ]
-    ];
-
     /**
-     * @Route("/list/{page}", name="blog_list", defaults={"page":"1"}, requirements={"page"="\d+"})
+     * @Route("/{page}", name="blog_list", defaults={"page":"1"}, requirements={"page"="\d+"})
      */
     public function list($page, Request $request)
     {
         $limit = $request->get('limit', 10);
+
+        $repository = $this->getDoctrine()->getRepository(BlogPost::class);
+        $items = $repository->findAll();
+
         return $this->json(
             [
             'page' => $page,
             'limit'=> $limit,
-            'data' => array_map(function ($item) {
-                return $this->generateUrl('blog_by_slug', ['slug'=>$item['slug']]);
-            },self::POSTS)
+            'data' => array_map(function (BlogPost $item) {
+                return $this->generateUrl('blog_by_slug', ['slug'=>$item->getSlug()]);
+            },$items)
             ]);
     }
 
     /**
-     * @Route("/post/{id}", name="blog_by_id", requirements={"id"="\d+"})
+     * @Route("/post/{id}", name="blog_by_id", requirements={"id"="\d+"}, methods={"GET"})
+     * @ParamConverter("post", class="App:BlogPost")
      */
-    public function post($id)
+    public function post($post)
     {
-        return $this->json(
-            self::POSTS[array_search($id, array_column(self::POSTS, 'id'))]
-        );
+        //It's the same than doing $this->getDoctrine()->getRepository(BlogPost::class)->find($id)
+        return $this->json($post);
     }
 
     /**
-     * @Route("/post/{slug}", name="blog_by_slug")
+     * @Route("/post/{slug}", name="blog_by_slug", methods={"GET"})
+     * The annotation below is not required when $post is typehinted with BlogPost and route parameter name matches
+     * any field on the BlogPost entity
+     * @ParamConverter("post", class="App:BlogPost", options={"mapping":{"slug":"slug"}})
      */
-    public function postBySlug($slug)
+    public function postBySlug($post)
     {
-        return $this->json(
-            self::POSTS[array_search($slug, array_column(self::POSTS, 'slug'))]
-        );
+        //It's the same than doing $this->getDoctrine()->getRepository(BlogPost::class)->findBy(['slug' => $slug])
+        return $this->json($post);
     }
 
-        /**
-         * @Route("/add", name="blog_add", methods={"POST"})
-         */
-        public function add(Request $request)
-        {
-            /** @var Serializer $serializer */
-            $serializer = $this->get('serializer');
+    /**
+     * @Route("/add", name="blog_add", methods={"POST"})
+     */
+    public function add(Request $request)
+    {
+//            $normalizer = new ObjectNormalizer();
+//            $encoder = new JsonEncoder();
+//
+//            $serializer = new Serializer(array($normalizer), array($encoder));
 
-            $blogPost = $serializer->deserialize($request->getContent(), BlogPost::class, 'json');
+        /** @var Serializer $serializer */
+        $serializer = $this->get('serializer');
 
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($blogPost);
-            $em->flush();
+        $blogPost = $serializer->deserialize($request->getContent(), BlogPost::class, 'json');
 
-            return $this->json($blogPost);
-        }
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($blogPost);
+        $em->flush();
+
+        return $this->json($blogPost);
+    }
+
+/**
+ * @Route("/post/{id}", name="blog_delete", methods={"DELETE"})
+ */
+    public function delete(BlogPost $post)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($post);
+        $em->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
 }
--- skeleton/src/Entity/BlogPost.php
+++ skeleton/src/Entity/BlogPost.php
@@ -2,10 +2,14 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\BlogPostRepository")
+ * @ApiResource()
  */
 class BlogPost
 {
@@ -32,7 +36,8 @@ class BlogPost
     private $content;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="posts")
+     * @ORM\JoinColumn(nullable=false)
      */
     private $author;
 
@@ -40,6 +45,22 @@ class BlogPost
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $slug;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Comment", mappedBy="blogPost")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $comments;
+
+    public function __construct()
+    {
+        $this->comments = new ArrayCollection();
+    }
+
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
 
     public function getId(): ?int
     {
@@ -82,18 +103,6 @@ class BlogPost
         return $this;
     }
 
-    public function getAuthor(): ?string
-    {
-        return $this->author;
-    }
-
-    public function setAuthor(string $author): self
-    {
-        $this->author = $author;
-
-        return $this;
-    }
-
     /**
      * @return mixed
      */
@@ -108,6 +117,25 @@ class BlogPost
     public function setSlug($slug): void
     {
         $this->slug = $slug;
+    }
+
+    /**
+     * @return User
+     */
+    public function getAuthor(): User
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param User $author
+     * @return self
+     */
+    public function setAuthor(User $author): self
+    {
+        $this->author = $author;
+
+        return $this;
     }
 
 }
